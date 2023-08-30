b71ec9b05db8489446b1dcc8db066f5727d06de5
thxi <thxixl@protonmail.com>
2023-08-30 13:58:39 +0100

    make weights stored as transposed

--- vitis_hls/transformer/transformer.cpp
+++ vitis_hls/transformer/transformer.cpp
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 
+#include <iostream>
+
+#include "scaled_dp_params.h"
+
 void print_mat(const dout_t* mat, int T1, int T2) {
   for (int i = 0; i < T1; i++) {
     for (int j = 0; j < T2; j++) {
@@ -9,4 +13,28 @@ void print_mat(const dout_t* mat, int T1, int T2) {
     }
     printf("\n");
   }
+}
+
+#define PRINT_INTERMEDIATE_RESULTS
+
+using namespace std;
+
+dout_t transformer(din_t xx[SEQ_LEN][INPUT_LEN]) {
+  // for every element in the sequence, apply front linear
+  // front_linear is 8x2 but it is stored as a transposed matrix
+  // so multiply on the right
+  // xx is 8x2
+  // so yy is 8x8
+  dout_t front_linear_output[SEQ_LEN][8] = {0};
+
+  kernel_mmult<dout_t, 8, 2, 8>(xx, FRONT_LINEAR_WEIGHT, front_linear_output);
+  kernel_bias_add<dout_t, 8, 8>(front_linear_output, FRONT_LINEAR_BIAS,
+                                front_linear_output);
+
+#ifdef PRINT_INTERMEDIATE_RESULTS
+  cout << "front_linear_output: " << endl;
+  print_mat((dout_t*)front_linear_output, SEQ_LEN, 8);
+#endif
+
+  return 0;
 }
--- vitis_hls/transformer/transformer.h
+++ vitis_hls/transformer/transformer.h
@@ -1,25 +1,17 @@
-// attention layer implemented in c++
 #include <math.h>
+#include <stdio.h>
 
 // define dtypes used in layers
 typedef double din_t;
 typedef double dout_t;
 
-// define dimensions
-#define INPUT_LENGTH 6          // number of observations in input sentence
-#define INPUT_EMBEDDING_SIZE 4  // embedding dimension of the input sequence
+#define INPUT_LEN 2
+#define SEQ_LEN 8
+#define NUM_HEADS 2
+#define HEAD_DIM 4
 
-#define DIM_QUERY 5
-#define DIM_KEY 5  // have to be the same as DIM_QUERY
-#define DIM_VALUE 7
-// so that the Query, Key and Value matrices are
-// DIM_QUERY x INPUT_EMBEDDING_SIZE, etc.
-
-void attention(const dout_t query_mat[DIM_QUERY * INPUT_EMBEDDING_SIZE],
-               const dout_t key_mat[DIM_KEY * INPUT_EMBEDDING_SIZE],
-               const dout_t value_mat[DIM_VALUE * INPUT_EMBEDDING_SIZE],
-               const din_t x[INPUT_LENGTH * INPUT_EMBEDDING_SIZE],
-               dout_t output[INPUT_LENGTH * DIM_VALUE]);
+// toplevel function
+dout_t transformer(din_t xx[SEQ_LEN][INPUT_LEN]);
 
 // for debugging
 void print_mat(const dout_t* mat, int T1, int T2);
@@ -31,6 +23,62 @@ void print_mat_template(const T mat[T1][T2]) {
       printf("%.6f ", mat[i][j]);
     }
     printf("\n");
+  }
+}
+
+// template <typename T, int M, int N, int K>
+// void kernel_mmult(T a[M * N], T b[N * K], T out[M * K]) {
+// loop_i:
+//   for (int i = 0; i < M; ++i) {
+// #pragma HLS PIPELINE off
+//   loop_j:
+//     for (int j = 0; j < N; ++j) {
+// #pragma HLS PIPELINE
+//       T sum = 0;
+//     loop_k:
+//       for (int k = 0; k < K; ++k) {
+// #pragma HLS UNROLL
+//         sum += a[i * N + k] * b[k * N + j];
+//       }
+//       out[i * M + j] = sum;
+//     }
+//   }
+//   return;
+// }
+
+template <typename T, int M, int N, int K>
+void kernel_mmult(const T a[M][N], const T b[N][K], T out[M][K]) {
+loop_i:
+  for (int i = 0; i < M; ++i) {
+#pragma HLS PIPELINE off
+  loop_j:
+    for (int j = 0; j < K; ++j) {
+#pragma HLS PIPELINE
+      T sum = 0;
+    loop_k:
+      for (int k = 0; k < N; ++k) {
+#pragma HLS UNROLL
+        sum += a[i][k] * b[k][j];
+      }
+      out[i][j] = sum;
+    }
+  }
+  return;
+}
+
+// basically, vector add
+template <typename T, int T1>
+void add_bias_to_row(const T* A, const T* bias, T* out) {
+  for (int i = 0; i < T1; i++) {
+    out[i] = A[i] + bias[i];
+  }
+}
+
+template <typename T, int M, int N>
+void kernel_bias_add(const T a[M][N], const T bias[N], T out[M][N]) {
+  for (int i = 0; i < M; i++) {
+#pragma HLS pipeline
+    add_bias_to_row<dout_t, N>(a[i], bias, out[i]);
   }
 }
 
@@ -76,13 +124,5 @@ void transpose(const T* A, T* result) {
 #pragma HLS pipeline off
       result[j * T1 + i] = A[i * T2 + j];
     }
-  }
-}
-
-template <typename T, int T1>
-void add_bias(T* A, const T* bias) {
-  for (int i = 0; i < T1; i++) {
-#pragma HLS pipeline off
-    A[i] += bias[i];
   }
 }
